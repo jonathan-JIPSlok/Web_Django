d8788be426d441f0fc0ea29dcfcd9d8ffb5c0639
Jonathan Yuri <jonathanpoli17@gmail.com>
2023-01-05 21:59:43 -0300

    Escrevendo codigo html no App

--- paginaOne/views.py
+++ paginaOne/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Olá Mundo!")
+    return HttpResponse("""
+    <h1>Olá Mundo!</h1>
+    <p>Meu nome é Jonathan Yuri e esse é meu inicio do aprendizado.</p>
+    """)
