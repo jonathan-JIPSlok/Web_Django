3068fbc81aca91693ea53bf257d1fe7f430a4903
Jonathan Yuri <jonathanpoli17@gmail.com>
2023-01-08 21:04:31 -0300

    Iniciando o servidor e entrando no admin

--- paginaOne/models.py
+++ paginaOne/models.py
@@ -10,6 +10,10 @@ class Question(models.Model):
 
     def __str__(self) -> str:
         return self.question_text
+    
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
@@ -19,6 +23,3 @@ class Choice(models.Model):
 
     def __str__(self) -> str:
         return self.choice_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
